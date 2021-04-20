b16ceddcb9ef7229c9349245a818a58d522c0c07
Jestin Mathews Kurian <70458425+JestinMK@users.noreply.github.com>
2021-04-20 19:19:33 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.isi.songify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread thread =  new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(2000);
+                    Intent intent= new Intent(MainActivity.this,SongList.class);
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        thread.start();
+
+
+    }
+}
